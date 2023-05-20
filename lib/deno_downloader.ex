8f30ae7f6f45beb2d29ea2a198c6844849ed3d56
Amos King <amos@binarynoggin.com>
2023-05-20 18:34:28 -0400

    Updates to support Deno v1.33.4 (#25)
    I didn't try to support all of the versions between each spot since they
    are all minor versions.

--- lib/deno_downloader.ex
+++ lib/deno_downloader.ex
@@ -4,9 +4,14 @@ defmodule DenoEx.DenoDownloader do
   """
 
   use OctoFetch,
-    latest_version: "1.33.1",
+    latest_version: "1.33.4",
     github_repo: "denoland/deno",
     download_versions: %{
+      "1.33.4" => [
+        {:darwin, :arm64, "ea504cac8ba53ef583d0f912d7834f4bff88eb647cfb10cb1dd24962b1dc062d"},
+        {:darwin, :amd64, "1e2d79b4a237443e201578fc825052245d2a71c9a17e2a5d1327fa35f9e8fc0e"},
+        {:linux, :amd64, "2e62448732a8481cae7af6637ddd37289e5baa6f22cd8e2f8197e25991869257"}
+      ],
       "1.33.1" => [
         {:darwin, :arm64, "e4a531d061fa8151070a6323e35a23620d6889297b810b6424a5125842ecbb47"},
         {:darwin, :amd64, "20166aa1b24b89edfa7b7c34f1b940f52bf8300108178cd37a36e5dd8a899c36"},
